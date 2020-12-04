1e32831d4e64e2a240d62a3d5a4a76a5bcb9c794
Swetha Deshmukh <swethadeshmukh677@gmail.com>
2020-12-04 12:55:25 -0500

    springboot demo

---
+++ demoClass/src/main/java/com/demoPackage/demoSpringboot/demoClass/DemoClassApplication.java
@@ -0,0 +1,13 @@
+package com.demoPackage.demoSpringboot.demoClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoClassApplication.class, args);
+	}
+
+}
---
+++ demoClass/src/test/java/com/demoPackage/demoSpringboot/demoClass/DemoClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoPackage.demoSpringboot.demoClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
